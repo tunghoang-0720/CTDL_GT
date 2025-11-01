20f117976370b05ce400fd03d836892e4e795c5d
tunghoang-0720 <tung.hv2419012@sis.hust.edu.vn>
2025-11-02 06:43:11 +0700

    bai 1

--- knap.cpp
+++ knap.cpp
@@ -19,6 +19,35 @@ int knapsack(vector<int>& w, vector<int>& v, int W, int n, vector<vector<int>>& 
     }
     return dp[n][W];
 }
+void TruyVet(vector<vector<int>>& dp, vector<int>& w, vector<int>& v, int n, int W)
+{
+    int i = n;
+    int wHienTai = W;
+    vector<int> monlay;
+
+    while (i > 0 && wHienTai > 0)
+    {
+        if (dp[i][wHienTai] != dp[i-1][wHienTai])
+        {
+            monlay.push_back(i);
+            wHienTai -= w[i];
+        }
+        i--;
+    }
+    
+
+    if(monlay.empty()) 
+        cout << "Khong lay mon nao" << endl;
+    else
+    {
+        cout << "lay cac mon sau: ";
+        for (int j = monlay.size()-1; j>=0; j--)
+        {
+            cout << monlay[j] <<" ";
+        }
+    }
+    cout << endl;    
+}
 
 int main()
 {
@@ -31,5 +60,6 @@ int main()
 
     vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
     cout << "Max value: " << knapsack(w, v, W, n, dp) << endl;
+    TruyVet(dp, w, v, n, W);
     return 0;
 }
