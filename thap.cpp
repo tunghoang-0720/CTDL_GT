2305cde03c755739bf4a817557ff5864b3d0a9c8
tunghoang-0720 <tung.hv2419012@sis.hust.edu.vn>
2025-11-01 19:13:04 +0700

    bai toan thap ha noi

--- thap.cpp
+++ thap.cpp
@@ -1,6 +1,31 @@
-#include<iostream>
-using namespace std;        
-int main() {
-    cout << "This is thap.cpp file!" << endl;
+#include <iostream>
+using namespace std;
+int buoc;
+
+void thap(int n, char nguon, char dich, char phu)
+{
+    if (n ==1)
+    {
+        buoc++;
+        cout << "Buoc: " << buoc << "Chuyen dia tu " << nguon << " sang " << phu << endl;
+    }
+    else 
+    {
+        thap(n-1, nguon, phu, dich);
+        
+        buoc++ ;
+        
+        cout << "Buoc: " << buoc << "Chuyen dia " << n << " tu " << nguon << " sang " << dich << endl;
+        
+        thap(n-1, phu, dich, nguon);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    thap(n, 'A', 'C', 'B');
+    cout << "tong so buoc can thiet la: " << buoc << endl;
     return 0;
 }
